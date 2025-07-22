8b97e99023150533312f03f877c6acb073ce7e71
Kenwaves <kenyaparris@gmail.com>
2025-07-21 21:28:35 -0400

    Merges and tidies data from UCI HAR

--- run_analysis.R
+++ run_analysis.R
@@ -5,9 +5,23 @@ y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
 subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
 x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
 y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+
 subjects <- rbind(subject_test,subject_train)
-set <- rbind(x_test, x_train)
+sets <- rbind(x_test, x_train)
 labels <- rbind(y_test, y_train)
-mdata <- cbind(subjects,set,labels)
-features_names <- read.table("UCI HAR Dataset/features.txt")
-colnames(mdata) <- c("subject", "activity", as.character(features_names$V2))
+
+mdata <- cbind(subjects, labels, sets)
+
+features <- read.table("UCI HAR Dataset/features.txt")
+colnames(mdata) <- c("subject", "activity", features$V2)
+mean_std<- grep("mean\\(\\)|std\\(\\)", features$V2)
+cols_keep <- c(1,2,  mean_std +2)
+
+mdata_clean <- mdata[,cols_keep]
+
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+mdata_clean$activity <- activity_labels[mdata_clean$activity,2]
+
+tidy_data <- mdata_clean %>%
+    group_by(subject,activity) %>%
+    summarise(across(everything(),mean))
